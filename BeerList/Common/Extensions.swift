cbcb98155e560f4929cf27213e5971b6676ff3d5
Roberto <herranzroberto15@gmail.com>
2021-09-21 15:06:34 +0200

    Base TableView

---
+++ BeerList/Common/Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  Extensions.swift
+//  BeerList
+//
+//  Created by Roberto Herranz on 21/9/21.
+//
+
+import UIKit
+
+extension UIView {
+    static let identifier = description()
+}
---
+++ BeerList/Common/Views/BeerCell.swift
@@ -0,0 +1,12 @@
+//
+//  BeerCell.swift
+//  BeerList
+//
+//  Created by Roberto Herranz on 21/9/21.
+//
+
+import UIKit
+
+class BeerCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+}
